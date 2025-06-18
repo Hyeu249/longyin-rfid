b34be5585804b61b8c52ba8d4c8432e27d61b307
hieu <labaohieu@gmail.com>
2025-06-18 10:21:37 +0700

    wip

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,17 +1,70 @@
 import React, { useEffect, useState } from "react";
 import { Alert, Button, Text, View } from "react-native";
+import C72RfidScanner from "../C72RfidScanner";
 
 export default function App() {
   const [isReading, setIsReading] = useState<boolean>(false);
   const [powerState, setPowerState] = useState<string>("");
   const [tags, setTags] = useState<string[]>([]);
 
-  const startSystem = async () => {};
-  const stopSystem = async () => {};
-  const readPower = async () => {};
-  const scanSingleTag = async () => {};
-  const startReading = async () => {};
-  const stopReading = async () => {};
+  const powerListener = (args: any[]): void => {
+    const data = args[0] as string;
+    setPowerState(data);
+  };
+
+  const tagListener = (args: any[]) => {
+    const [epc] = args; // lấy EPC từ mảng
+    setTags((prevTags) => [...prevTags, epc]);
+  };
+
+  useEffect(() => {
+    C72RfidScanner.powerListener?.(powerListener);
+    C72RfidScanner.tagListener?.(tagListener);
+
+    return () => {
+      C72RfidScanner.deInitializeReader?.();
+    };
+  }, []);
+
+  const startSystem = async () => {
+    C72RfidScanner.initializeReader?.();
+  };
+  const stopSystem = async () => {
+    C72RfidScanner.deInitializeReader?.();
+  };
+
+  const readPower = async () => {
+    try {
+      const result = await C72RfidScanner.readPower();
+      console.log(`The result is ${result}`);
+    } catch (error: any) {
+      console.log(error.message || "Unknown error");
+    }
+  };
+
+  const scanSingleTag = async () => {
+    try {
+      const result = await C72RfidScanner.readSingleTag();
+      console.log(`The result is ${result}`);
+      console.log(result);
+    } catch (error: any) {
+      console.log(error.message || "Unknown error");
+    }
+  };
+  const startReading = () => {
+    C72RfidScanner.startReadingTags?.((args: any[]) => {
+      const message = args[0] as boolean;
+      setIsReading(message);
+    });
+  };
+
+  const stopReading = () => {
+    C72RfidScanner.stopReadingTags?.((args: any[]) => {
+      setIsReading(false); // bạn đang hardcode `false` nên không cần dùng `args` nếu không cần
+    });
+
+    console.log("Collected Tags:", tags);
+  };
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
@@ -20,7 +73,7 @@ export default function App() {
       </View>
 
       <View style={{ marginVertical: 20 }}>
-        <Button onPress={startSystem} title="Start System" />
+        <Button onPress={startSystem} title="Start Systems" />
       </View>
 
       <View style={{ marginVertical: 20 }}>
